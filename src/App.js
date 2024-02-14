96582bc8866db96cdc278e0a1019b906ea193ea7
arpit3334 <93903732+arpit3334@users.noreply.github.com>
2024-02-14 08:57:02 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import {ethers} from "ethers";
+import Upload from "./artifacts/contracts/Upload.sol/Upload.json"
+import FileUpload from "./components/FileUpload"
+import Modal from "./components/Modal"
+import Display from "./components/Display"
 import './App.css';
 
 function App() {
+  const [account, setAccount]=useState('');
+  const [contract, setContract]=useState(null);
+  const [provider, setProvider]=useState(null);
+  const [modalOpen, setModalOpen]=useState(false);
+
+
+  useEffect(()=>{
+    const provider =new ethers.BrowserProvider(window.ethereum);
+    const wallet = async ()=>{
+
+      if(provider){
+          await provider.send("eth_requestAccounts",[]);
+          const signer = await provider.getSigner();
+          const address = await signer.getAddress();
+          console.log(address);
+          setAccount(address);
+          const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+          const contract =new ethers.Contract(
+            contractAddress,
+            Upload.abi,
+            signer
+          );
+          console.log(contract);
+          setContract(contract);
+          setProvider(signer);
+      }else{
+        alert("Metamask not installed");
+      }
+    }
+    provider && wallet();
+  },[])
+
+
   return (
+    <>
+    {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 style={{ color: "white" }}>MyDrive 3.0</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "white" }}>
+          Account : {account ? account : "Not connected"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+        </div></>
   );
 }
 
